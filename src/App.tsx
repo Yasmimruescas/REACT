7895e6d784a9566c7f31a25d6b89a1bc9721dacb
Yasmim <yasmim.ruescas@outlook.com>
2025-05-23 11:51:11 -0300

    fix: Ajuste da pasta home

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/NavBar';
-import Home from './home/Home'
+import Home from './pages/home/Home';
 import Cadastro from './pages/cadastro/Cadastro';
 import Login from './pages/Login/Login';
 import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <>
     <AuthProvider>
-        <BrowserRouter>
-          <Navbar />
-          <div className="min-h-[80vh]">
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-        </AuthProvider>
-    </>
-  )
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
